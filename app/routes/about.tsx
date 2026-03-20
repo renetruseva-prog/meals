ec94dea669f55ff99c010bd71bf85bbb443f3498
ruseva <renet.ruseva@student.howest.be>
2026-03-20 10:00:40 +0100

    we created a grid of possible meals

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,13 +1,28 @@
-export default function About() {
-    return <div>
-        <ul>
-            <li>
-                <div>
-                    <p>Category: {strCategory}</p>
-                    <img src={strCategoryThumb}></img>
-                    <p>Description: {strCategoryDescription}</p>
-                </div>
-            </li>
-        </ul>
-    </div>
+export default function About({ loaderData }) {
+    return (
+        <div className="container">
+            <ul className="grid">
+                {loaderData.map((cat) => (
+                    <li key={cat.idCategory} className="card">
+                        <img
+                            src={cat.strCategoryThumb}
+                            alt={cat.strCategory}
+                            className="card-img"
+                        />
+                        <div className="card-content">
+                            <h2>{cat.strCategory}</h2>
+                            <p>{cat.strCategoryDescription}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export async function clientLoader({ params }: Route.LoaderArgs) {
+    // const mealId = params.mealId;
+    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`);
+    const data = await res.json();
+    return data.categories;
 }
